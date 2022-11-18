4a071da8b6245ec76e4e064d52754960447065a6
MiguelAnchico <mangel.anchico@gmail.com>
2022-11-18 13:43:51 -0500

    Agarrar y soltar Completados (Falta ajustes)

---
+++ Assets/Scrip/AdjustPosition.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjustPosition : MonoBehaviour {
+	public GameObject parent;
+	public Vector3 gap;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = this.parent.transform.position + gap;
+	}
+}
---
+++ Assets/getObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getObject : MonoBehaviour {
+
+	public GameObject objectToTake;
+
+	// Use this for initialization
+	void Start () {
+		objectToTake = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("Agarrable") && objectToTake == null){
+			objectToTake = other.gameObject;
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if(GameObject.ReferenceEquals(objectToTake, other.gameObject)){
+			objectToTake = null;
+		}
+	}
+}
