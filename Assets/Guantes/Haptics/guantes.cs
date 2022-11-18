849fae39c7f4140144bc96cb081e893bdf8b0902
Diego Ortiz Hurtado <73130246+museortizmedia@users.noreply.github.com>
2022-11-18 12:40:16 -0500

    game controller singleton y haptic controller

--- Assets/Guantes/Haptics/guantes.cs
+++ Assets/Guantes/Haptics/guantes.cs
@@ -8,46 +8,61 @@ using System;
 
 public class guantes : MonoBehaviour {
 
-	 string DERECHA ="COM5"; //puerto up
-	 string IZQUIERDA = "COM4"; //puerto dow
+	public string Puerto_DERECHA = "COM5"; //puerto up
+	public string Puerto_IZQUIERDA = "COM4"; //puerto down
 	SerialPort arduino; SerialPort arduino2;
-	public string Accionador;
+	public string[] Acc_Todos = new string[] { "I", "K", "M", "G", "A", "B"};
+	public string[] Acc_Medios = new string[] { "I" };
+	public string[] Acc_Corazones = new string[] { "K" };
+	public string[] Acc_Indices = new string[] { "M" };
+	public string[] Acc_Chiquitos = new string[] { "G" };
+	public string[] Acc_Pulgares = new string[] { "A" };
+	public string[] Acc_Centro = new string[] { "B" }; //solo derecha
+	public int VibracionLarga = 9, VibracionCorta = 3, VibracionBreve= 5;
 
 	void Start () {
-		arduino = new SerialPort(DERECHA, 9600);
-		arduino2 = new SerialPort(DERECHA, 9600);
-		arduino.Open();
-		arduino2.Open();
+		arduino = new SerialPort(Puerto_DERECHA, 9600);
+		arduino2 = new SerialPort(Puerto_IZQUIERDA, 9600);
+		try
+		{
+			arduino.Open();
+        }
+        catch { Debug.LogError("No se detectan el puerto derecha"); }
+		try
+		{
+			arduino2.Open();
+		}
+		catch { Debug.LogError("No se detectan el puerto izquierda"); }
 	}
 	
-	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Q))
 		{
 			if (arduino.IsOpen)
 			{
-				arduino.Write(Accionador);
-				arduino.Write(Accionador);
-				arduino.Write(Accionador);
-				arduino.Write(Accionador);
-				arduino.Write(Accionador);
-				arduino.Write(Accionador);
-				arduino.Write(Accionador);
-				arduino.Write(Accionador);
-				arduino.Write(Accionador);
+				Vibrate(Acc_Todos, VibracionLarga, 0);
 			}
 			if (arduino2.IsOpen)
 			{
-				arduino2.Write(Accionador);
-				arduino2.Write(Accionador);
-				arduino2.Write(Accionador);
-				arduino2.Write(Accionador);
-				arduino2.Write(Accionador);
-				arduino2.Write(Accionador);
-				arduino2.Write(Accionador);
-				arduino2.Write(Accionador);
-				arduino2.Write(Accionador);
+				Vibrate(Acc_Todos, VibracionLarga, 0);
 			}
 		}
 	}
+
+	public void Vibrate(string[] referencias, int rep, int mano = 0)
+	{
+		SerialPort[] ardu;
+		ardu = mano >= 1 ? new SerialPort[] { arduino } : mano <= -1 ? new SerialPort[] { arduino2 }  : new SerialPort[] { arduino, arduino2 };
+		for (int j = 0; j < rep; j++) 
+			{
+				for (int i = 0; i < referencias.Length; i++)
+				{
+					for (int k = 0; k < ardu.Length; k++)
+					{
+						ardu[k].Write(referencias[i]);
+					}
+				}
+			}
+	}
+
 }
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController Instance;
+	public guantes HapxelController;
+	public bool activo = false;
+
+	void Awake() {
+		if (Instance == null )
+		{
+			Instance = this;
+			DontDestroyOnLoad(this.gameObject);
+		}
+		else
+		{
+			Destroy(this);
+		}
+	}
+	void Start () {
+		HapxelController = gameObject.GetComponent<guantes>();	
+	}
+	
+	void Update () {
+	}
+}
