d329b3e3693a4d7fefb62d011b3bc12e4e669129
Diego Ortiz Hurtado <73130246+museortizmedia@users.noreply.github.com>
2022-11-19 12:11:20 -0500

    audios recorter

--- Assets/animplayer.cs
+++ Assets/animplayer.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class animplayer : MonoBehaviour {
 
     public void startAudioInicio() { GameController.Instance.durningAnimacionInicio(); }
+    public void endAudioInicio() { GameController.Instance.endAnimacionInicio(); }
     public void startAudioFin() { GameController.Instance.duringAnimacionFinal(); }
 
 }
