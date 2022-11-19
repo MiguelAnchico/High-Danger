4808ab4782b780978d400f1c69f137d76f25cf19
MiguelAnchico <mangel.anchico@gmail.com>
2022-11-19 11:05:08 -0500

    Scene Dead Canvas update

---
+++ Assets/Scrip/GeneratorCauticLogic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneratorCauticLogic : MonoBehaviour {
+
+	public GameObject Cautic;
+
+	// Use this for initialization
+	void Start () {
+		Generate ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Generate(){
+		Instantiate (Cautic);
+	}
+}
