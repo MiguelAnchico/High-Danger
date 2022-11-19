c289878110690cfb1e16df03c286aaeaf3b4a0b5
MiguelAnchico <mangel.anchico@gmail.com>
2022-11-19 08:16:55 -0500

    SongController Complete

---
+++ Assets/Scrip/SongController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SongController : MonoBehaviour {
+	public static SongController Instance;
+	public AudioSource[] audios;
+	public AudioClip[] songs;
+
+	private int SonidoAmbiente;
+	private int SonidoElectrocucion;
+	private int SonidoDeMuerte;
+	private int SonidoSoldar;
+	private int SonidoViento;
+
+	// Use this for initialization
+	void Start () {
+		SonidoAmbiente = 0;
+		SonidoElectrocucion = 1;
+		SonidoDeMuerte = 2;
+		SonidoSoldar = 3;
+		SonidoViento = 4;
+
+		audios [SonidoAmbiente].clip = songs [SonidoAmbiente];
+		audios [SonidoElectrocucion].clip = songs [SonidoElectrocucion];
+		audios [SonidoDeMuerte].clip = songs [SonidoDeMuerte];
+		audios [SonidoSoldar].clip = songs [SonidoSoldar];
+		audios [SonidoViento].clip = songs [SonidoViento];
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void playSound(int audioPosition){
+		
+		audios [audioPosition].Play ();
+	}
+
+	void stopSound(int audioPosition){
+		audios[audioPosition].Stop ();
+	}
+}
